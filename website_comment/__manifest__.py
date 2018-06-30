a414951da426be0cc608c204256f7eed1c3c9cd4
Odoo Aktiv <solankiriddhir@yahoo.in>
2018-06-30 15:02:04 +0530

    [IMP] pep8 standards for branch 11.0

--- website_comment/__manifest__.py
+++ website_comment/__manifest__.py
@@ -7,12 +7,13 @@
     'website': 'http://www.aktivsoftware.com',
     'author': 'Aktiv Software',
     'license': 'AGPL-3',
-    'description': 'The purpose of this module is, it helps to add a comment box on the website checkout and by confirming details it will gets saved too.',
+    'description': 'The purpose of this module is, it helps to add a comment box\
+    on the website checkout and by confirming details it will gets saved too.',
     'depends': ['base', 'sale', 'website_sale'],
     'data': [
         'views/sale_order_inherit_view.xml',
         'static/src/xml/website_sale_order_template.xml',
-	'report/sale_report_template_inherited.xml',
+        'report/sale_report_template_inherited.xml',
     ],
     'images': ['static/description/banner.jpg'],
     'auto_install': False,
